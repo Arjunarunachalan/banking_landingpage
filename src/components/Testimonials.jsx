5e4cacab86b0d00560bda9f8c0be0e2128716f79
arjun <arjunarunachalan@gmail.com>
2023-08-24 23:49:13 +0530

    fixed the responsible issue in testimonials by update the blue gradient margin

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => (
     <section className={`${styles.paddingY} ${styles.flexCenter} flex-col first-letter:relative`}>
      {/* todo */}
      <div className="absolute z-[0] w-[60%] h-[60%]
-     -right-[50%] rounded-full blue__gradient"/>
+     right-[80%] rounded-full blue__gradient "/>
 
      <div className="w-full flex justify-between 
      items-center md:flex-row flex-col sm:mb-16 mb-6 
