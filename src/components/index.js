61d686c1d3f5cdbea9fd98779c15c57b670c3dd5
arjun <arjunarunachalan@gmail.com>
2023-08-24 23:11:30 +0530

    banking landing page

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from "./NavBar";
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+import FeedBack from "./FeedBack";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import CarDeal from "./CarDeal";
+import Button from "./Button";
+import Buisness from "./Buisness";
+import Billing from "./Billing";
+
+
+export {
+    Navbar,
+    Billing,
+    CarDeal,
+    Buisness,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    FeedBack,
+    Button,
+    GetStarted
+}
